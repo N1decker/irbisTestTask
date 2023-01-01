0bdbeeba0ad74dd10503bde952cfd5b50dd64797
Egor Ershov <eg0r.ershoff@yandex.ru>
2023-01-01 11:24:39 +0300

    add oauth2 login and move constants to application.yml

--- src/main/java/com/nidecker/testtask/util/AppConstants.java
+++ src/main/java/com/nidecker/testtask/util/AppConstants.java
@@ -1,7 +1,10 @@
 package com.nidecker.testtask.util;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AppConstants {
 
-    public static final String DEFAULT_PAGE_NUMBER = "0";
-    public  static final String DEFAULT_PAGE_SIZE = "5";
+    public static final String DEFAULT_PAGE_NUMBER = "${default-page-number}";
+    public  static final String DEFAULT_PAGE_SIZE = "${default-page-size}";
 }
